85a289cd300859b1bfd2f7ed0cef3bbae46afe44
Hosszú Tamás <hosszutamas@ktch.hu>
2025-01-28 17:37:11 +0100

    Add project files.

---
+++ api/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace api.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Coins { get; set; }
+        public int HighScore { get; set; }
+
+    }
+}
